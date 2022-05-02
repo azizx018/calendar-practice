8795cecbe0def420ec0c35d0c82c1b8f155a5c19
azizx018 <chiara.aziz@gmail.com>
2022-05-02 14:29:38 -0500

    simplified evemtlist test and comp

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -2,12 +2,7 @@ import Event from "./Event";
 
 export default function EventList({events, _Event=Event}){
     return <>
-        {events.map((event, index) => <_Event key={index}
-            title={event.title}
-            date={event.date}
-            description={event.description}
-            complete={event.complete}
-        />)}
+        {events.map((event, index) => <_Event key={index} event={event}/>)}
     </>
 
 }
--- src/components/EventList.test.js
+++ src/components/EventList.test.js
@@ -4,23 +4,10 @@ import EventList from "./EventList";
 it('should display 2 events', () => {
     const yesterday = new Date()
     yesterday.setDate(yesterday.getDate() - 1)
-    const eventData = [
-        'event1',
-        'event2'
-    ]
-
-    function mockEvent({event}){
-        return <>{event}
-        </>
-
-    }
+    const eventData = ['event1', 'event2']
+    const mockEvent = ({event}) => <div>{event}</div>
     render(<EventList events={eventData} _Event={mockEvent}/>)
-    expect(screen.getByText('party1')).toBeInTheDocument()
-    expect(screen.getByText('party2')).toBeInTheDocument()
-    expect(screen.getByText('desc1')).toBeInTheDocument()
-    expect(screen.getByText('desc2')).toBeInTheDocument()
-    expect(screen.getByText('Complete')).toBeInTheDocument()
-    expect(screen.getByText('Upcoming')).toBeInTheDocument()
-    expect(screen.getByText(eventData[0].date.toDateString())).toBeInTheDocument()
-    expect(screen.getByText(eventData[1].date.toDateString())).toBeInTheDocument()
+    expect(screen.getByText('event1')).toBeInTheDocument()
+    expect(screen.getByText('event2')).toBeInTheDocument()
+
 })
